704d1d0fa12c87f27b5c61a49b94b44abd48c92c
Kevin Laptop <kevinyu37979@gmail.com>
2024-07-05 10:36:28 -0700

    added perimeter and area triangle calculators

---
+++ day_3/operators.py
@@ -0,0 +1,21 @@
+#Day 3 Exercises - Operators
+
+#Variable Declaration
+age = int(26)
+height = float(5.833)
+complex_number = 1+1j
+
+#triangle area calculator
+print("First, we will calculate a triangle's area.")
+height = float(input('Enter triangle height: '))
+base = float(input('Enter triangle length: '))
+area = 0.5 * height * base
+print(area)
+
+#triangle perimeter calculator
+print('Next, we are going to calculate the perimeter.')
+side_1 = float(input('Enter side a: '))
+side_2 = float(input('Enter side b: '))
+side_3 = float(input('Enter side c: '))
+print("The perimeter of the triangle is: " + str(side_1+side_2+side_3))
+
