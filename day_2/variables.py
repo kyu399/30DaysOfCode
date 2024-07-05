52f626a95a2337f60cde4fab05c99718113e63b3
Kevin Laptop <kevinyu37979@gmail.com>
2024-07-05 09:30:33 -0700

    declared variables in accordance with exercise level 1

---
+++ day_2/variables.py
@@ -0,0 +1,13 @@
+# Variables List
+first_name = 'Kevin'
+last_name = 'Yu'
+full_name = first_name + " " + last_name
+country = 'USA'
+city = 'Around Daly City'
+age = 26
+is_married = False
+year = 2024 - age
+is_true = True
+is_light_on = True
+first_name_2, last_name_2 = 'Tester','Two'
+
